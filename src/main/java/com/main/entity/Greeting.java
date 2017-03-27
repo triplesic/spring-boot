a3a6a7505084e2e1aa6a5224e5845e23daa88904
THEERAWAT KAEWCHOTE <theerawatkc@gmail.com>
2017-03-27 11:37:58 +0700

    hello world

---
+++ src/main/java/com/main/entity/Greeting.java
@@ -0,0 +1,28 @@
+package com.main.entity;
+
+public class Greeting {
+	
+	private long id;
+	private String message;
+	
+	public Greeting (long id,String message){
+		this.id = id;
+		this.message = message;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
